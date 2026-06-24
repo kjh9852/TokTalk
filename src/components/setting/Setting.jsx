dbc7217431ea6f13a993b6efe4c035725d8c8420
JeongHyeonKim <kjh985264@gmail.com>
2026-06-24 20:58:44 +0900

    feat: 사용자 설정 컴포넌트 추가
    - ToggleButton을 활용한 한국어/영어(KR/EN) 전역 언어 스위칭 기능 구현
    - Socket.io 이벤트를 연동한 관리자 인증 및 권한 해제 로직 적용

---
+++ src/components/setting/Setting.jsx
@@ -0,0 +1,76 @@
+import { useState } from "react";
+
+import { useAdmin } from "@/context/AdminContext";
+import { useLanguage } from "@/context/LanguageContextProvider";
+
+import AdminForm from "@/components/form/AdminForm";
+import Button from "@/components/ui/Button/Button";
+import ToggleButton from "@/components/ui/ToggleButton/ToggleButton";
+
+import styles from "./Setting.module.css";
+
+export default function Setting({ socket }) {
+  const { isAdmin, setIsAdmin } = useAdmin();
+  const [adminMode, setAdminMode] = useState(isAdmin);
+  const [adminCode, setAdminCode] = useState("");
+  const { t, toggleLanguage, isOn, setIsOn } = useLanguage();
+
+  const handleAdminCodeChange = (e) => {
+    setAdminCode(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("enterAdminEnter", adminCode);
+    socket.once("adminConfirmed", () => {
+      setIsAdmin(true);
+    });
+  };
+
+  const handleExitAdmin = (e) => {
+    e.preventDefault();
+    socket.emit("exitAdminMode");
+    socket.once("exitAdminModeConfirmed", () => {
+      setIsAdmin(false);
+    });
+  };
+
+  const handleAdminMode = () => {
+    setAdminMode((prev) => (prev ? false : true));
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.toggleContainer}>
+        <ToggleButton
+          title={t.setting.language}
+          leftLabel="KR"
+          rightLabel="EN"
+          onClick={toggleLanguage}
+          isToggleOn={isOn}
+        />
+        <ToggleButton
+          title={t.setting.admin}
+          leftLabel="OFF"
+          rightLabel="ON"
+          onClick={handleAdminMode}
+          isToggleOn={isAdmin}
+        />
+      </div>
+      {adminMode && (
+        <AdminForm
+          isAdmin={isAdmin}
+          onSubmit={handleSubmit}
+          defaultValue={adminCode}
+          onChange={handleAdminCodeChange}
+        />
+      )}
+      {isAdmin && (
+        <div>
+          <p className={styles.text}>Admin Mode</p>
+          <Button onClick={handleExitAdmin}>해제</Button>
+        </div>
+      )}
+    </div>
+  );
+}
