cd8c459d967a404ac9617cac668e8fbc888b39e9
JeongHyeonKim <kjh985264@gmail.com>
2026-06-24 20:33:43 +0900

    feat: 언어 관련 Context 및 데이터 추가

---
+++ src/translations/translations.js
@@ -0,0 +1,79 @@
+export const translations = {
+  ko: {
+    modal: {
+      title: {
+        nickname: "닉네임 변경",
+        post: "게시글 등록",
+        info: "정보",
+        setting: "설정",
+        postedit: "게시글 관리",
+      },
+      button: {
+        nickname: "변경",
+        post: "등록",
+      },
+      label: {
+        nickname: "닉네임",
+        post: "게시글",
+      },
+      placeholder: {
+        nickname: "닉네임을 입력해주세요.",
+        post: "게시글을 입력해주세요.",
+      },
+    },
+    menuList: {
+      menu: "메뉴",
+      nickname: "닉네임 변경",
+      info: "정보",
+      setting: "설정",
+    },
+    chat: {
+      message: "메세지를 입력해주세요.",
+      button: "전송",
+    },
+    author: "작성자",
+    userCount: "접속자수",
+    setting: {
+      language: "언어설정",
+      admin: "관리자 모드",
+    },
+  },
+  en: {
+    modal: {
+      title: {
+        nickname: "Nickname Change",
+        post: "Create Post",
+        info: "Information",
+        setting: "Setting",
+      },
+      button: {
+        nickname: "Change",
+        post: "Create",
+      },
+      label: {
+        nickname: "Nickname",
+        post: "Post",
+      },
+      placeholder: {
+        nickname: "Enter your nickname",
+        post: "Please enter your post",
+      },
+    },
+    menuList: {
+      menu: "Menu",
+      nickname: "Nickname Change",
+      info: "Infomation",
+      setting: "Setting",
+    },
+    chat: {
+      message: "Please enter your message",
+      button: "Send",
+    },
+    author: "Author",
+    userCount: "User count",
+    setting: {
+      language: "Language setting",
+      admin: "Admin mode",
+    },
+  },
+};
