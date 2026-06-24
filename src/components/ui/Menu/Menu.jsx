791c84c36a12c0cb1756ff8ca7ee824fd9390817
JeongHyeonKim <kjh985264@gmail.com>
2026-06-24 21:05:56 +0900

    feat: 모달 타입 전환 및 서브 메뉴를 제어하는 Menu 컴포넌트 추가
    - 로컬 상태(isOpen)에 따른 서브 메뉴 리스트 토글 기능 구현

---
+++ src/components/ui/Menu/Menu.jsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+
+import InfomationIcon from "@/assets/icons/infomation.png";
+import MenuIcon from "@/assets/icons/menu.png";
+import SettingIcon from "@/assets/icons/setting.png";
+import UserIcon from "@/assets/icons/user.png";
+import { useLanguage } from "@/context/LanguageContextProvider";
+import { useModalContext } from "@/context/ModalContextProvider";
+
+import styles from "./Menu.module.css";
+
+export default function Menu() {
+  const { t } = useLanguage();
+  const { handleModalOpen, setModalType } = useModalContext();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleMenuOpen = (e) => {
+    e.stopPropagation();
+    setIsOpen((prev) => !prev);
+  };
+
+  const handleModalOpenWithType = (type) => (e) => {
+    e.stopPropagation();
+
+    setModalType(type);
+    handleModalOpen();
+  };
+
+  return (
+    <div className={styles.buttonPosition}>
+      <div className={styles.menuButton}>
+        <button className={styles.menuFlex} onClick={handleMenuOpen}>
+          <img src={MenuIcon} className={styles.menuImage} />
+          <div className={styles.menuName}>
+            <span>{t.menuList.menu}</span>
+          </div>
+        </button>
+      </div>
+      <div
+        className={`${styles.menuWrapper} ${
+          isOpen ? styles.open : styles.close
+        }`}
+      >
+        <ul className={styles.menu}>
+          <li className={styles.menuButton}>
+            <button
+              className={styles.menuFlex}
+              onClick={handleModalOpenWithType("nickname")}
+            >
+              <img src={UserIcon} className={styles.menuImage} />
+              <div className={styles.menuName}>
+                <span>{t.menuList.nickname}</span>
+              </div>
+            </button>
+          </li>
+          <li className={styles.menuButton}>
+            <button
+              className={styles.menuFlex}
+              onClick={handleModalOpenWithType("info")}
+            >
+              <img src={InfomationIcon} className={styles.menuImage} />
+              <div className={styles.menuName}>
+                <span>{t.menuList.info}</span>
+              </div>
+            </button>
+          </li>
+          <li className={styles.menuButton}>
+            <button
+              className={styles.menuFlex}
+              onClick={handleModalOpenWithType("setting")}
+            >
+              <img src={SettingIcon} className={styles.menuImage} />
+              <div className={styles.menuName}>
+                <span>{t.menuList.setting}</span>
+              </div>
+            </button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+}
