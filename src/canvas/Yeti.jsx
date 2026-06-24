44e78a7d9f343c7a81af993a0654e50005aa4f08
JeongHyeonKim <kjh985264@gmail.com>
2026-06-24 20:02:58 +0900

    feat: Yeti 오브젝트 구현

---
+++ src/canvas/Yeti.jsx
@@ -0,0 +1,47 @@
+import { useGLTF } from "@react-three/drei";
+
+export default function Yeti() {
+  const { nodes } = useGLTF("models/yeti.glb");
+
+  return (
+    <group dispose={null} scale={1}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube004.geometry}
+        material={nodes.Cube004.material}
+        scale={2}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube004_1.geometry}
+        material={nodes.Cube004_1.material}
+        scale={2}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube004_2.geometry}
+        material={nodes.Cube004_2.material}
+        scale={2}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube004_3.geometry}
+        material={nodes.Cube004_1.material}
+        scale={2}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.Cube004_4.geometry}
+        material={nodes.Cube004_4.material}
+        scale={2}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload("/yeti.glb");
