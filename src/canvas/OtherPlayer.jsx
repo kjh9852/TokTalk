717e974d04f359a59921a317b94ea0d300b43f6a
kjh9852 <kjh985264@gmail.com>
2026-06-29 05:18:57 +0900

    fix: OtherPlayer의 THREE 객체를 useRef로 최적화

--- src/canvas/OtherPlayer.jsx
+++ src/canvas/OtherPlayer.jsx
@@ -1,13 +1,10 @@
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
 
 import { useFrame } from "@react-three/fiber";
 import { CapsuleCollider, RigidBody } from "@react-three/rapier";
 import * as THREE from "three";
 
 import PlayerModel from "@/canvas/PlayerModel";
-
-const targetPos = new THREE.Vector3();
-const targetQuat = new THREE.Quaternion();
 
 export default function OtherPlayer({
   position,
@@ -19,11 +16,14 @@ export default function OtherPlayer({
 }) {
   const playerRef = useRef();
 
-  useFrame((state, delta) => {
+  const targetPos = useRef(new THREE.Vector3());
+  const targetQuat = useRef(new THREE.Quaternion());
+
+  useFrame((_, delta) => {
     if (!playerRef.current) return;
 
     if (position) {
-      targetPos.set(position[0], position[1], position[2]);
+      targetPos.current.set(position[0], position[1], position[2]);
 
       // 현재 라피어 물리 바디의 위치를 가져옵니다.
       const currentPos = playerRef.current.translation();
@@ -34,7 +34,7 @@ export default function OtherPlayer({
       );
 
       // 매 프레임마다 목적지를 향해 15%씩 미끄러지듯 이동 (0.15 값을 조절해 부드러움 설정 가능)
-      currentPosVec.lerp(targetPos, 1 - Math.exp(-12.5 * delta));
+      currentPosVec.lerp(targetPos.current, 1 - Math.exp(-12.5 * delta));
 
       // 보간된 위치를 물리 엔진 바디에 부드럽게 반영
       playerRef.current.setNextKinematicTranslation(currentPosVec);
@@ -42,7 +42,12 @@ export default function OtherPlayer({
 
     // 2. 부모(소켓)에게 받은 최신 회전 Quaternion 배열 적용
     if (rotation) {
-      targetQuat.set(rotation[0], rotation[1], rotation[2], rotation[3]);
+      targetQuat.current.set(
+        rotation[0],
+        rotation[1],
+        rotation[2],
+        rotation[3],
+      );
 
       // 현재 라피어 물리 바디의 회전값을 쿼터니언으로 변환
       const currentRot = playerRef.current.rotation();
@@ -54,7 +59,7 @@ export default function OtherPlayer({
       );
 
       // 회전도 부드럽게 slerp(구면 선형 보간) 처리
-      currentQuat.slerp(targetQuat, 0.15);
+      currentQuat.slerp(targetQuat.current, 0.15);
 
       playerRef.current.setNextKinematicRotation(currentQuat);
     }
@@ -67,7 +72,7 @@ export default function OtherPlayer({
       type={"kinematicPosition"}
       ref={playerRef}
       lockRotations
-      position={position ?? [0, 10, 0]}
+      position={position ?? [0, 11, 0]}
     >
       <CapsuleCollider args={[0.2, 0.4]} />
       <PlayerModel
