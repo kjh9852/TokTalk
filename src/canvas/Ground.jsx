f593f94c6af6a244cfef67ce84737ae0f6395ef6
JeongHyeonKim <kjh985264@gmail.com>
2026-07-04 22:36:12 +0900

    refactor: 사용되지 않는 로직 정리

--- src/canvas/Ground.jsx
+++ src/canvas/Ground.jsx
@@ -1,18 +1,11 @@
 import floorTexture from "@/assets/images/grid02.png";
-import { useModalContext } from "@/context/ModalContextProvider";
 import { useTexture } from "@react-three/drei";
 import { CuboidCollider, RigidBody } from "@react-three/rapier";
 import * as THREE from "three";
 
 export default function Ground() {
-  const { setModalType, handleModalOpen, handleModalClose } = useModalContext();
   const texture = useTexture(floorTexture);
   texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
-
-  const handlePostModalOpen = () => {
-    setModalType("post");
-    handleModalOpen();
-  };
 
   return (
     <RigidBody type="fixed" colliders={false}>
@@ -21,7 +14,7 @@ export default function Ground() {
         <meshStandardMaterial
           color="gray"
           map={texture}
-          map-repeat={[20, 20]}
+          map-repeat={[15, 15]}
         />
       </mesh>
       <CuboidCollider args={[25, 2, 25]} position={[0, -2, 0]} />
