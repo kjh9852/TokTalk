7a71198a84092eb9981264be22c68b9c0c92bec1
kjh9852 <kjh985264@gmail.com>
2026-07-07 17:40:19 +0900

    refactor: 게시글 페이지네이션 로직 개선
    - page state 컴포넌트 내부로 이동
    - pageDocs 관리 개선

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,15 +1,43 @@
-import { usePosts } from "@/context/PostContext.jsx";
+import { useEffect, useState } from "react";
+
+import { useGetPosts } from "@/queries/useGetPosts.js";
+import { useTotalPage } from "@/queries/useTotalPage.js";
 
 import Board from "@/canvas/Board.jsx";
 
 import PostList from "./PostList/PostList.jsx";
 
 export default function Post() {
-  const { postList, handleNextPage, handlePrevPage } = usePosts();
+  const [pageDocs, setPageDocs] = useState([null]);
+  const [page, setPage] = useState(1);
+  const { data } = useGetPosts(page, pageDocs);
+  const { data: totalPage } = useTotalPage();
+  const posts = data?.posts ?? [];
+
+  const handleNextPage = () => {
+    if (page >= totalPage) return;
+
+    setPage((prev) => prev + 1);
+  };
+
+  const handlePrevPage = () => {
+    if (page <= 1) return;
+
+    setPage((prev) => prev - 1);
+  };
+
+  useEffect(() => {
+    if (!data?.lastDoc) return;
+    setPageDocs((prev) => {
+      const newArr = [...prev];
+      newArr[page] = data.lastDoc;
+      return newArr;
+    });
+  }, [data]);
 
   return (
     <>
-      <PostList postList={postList} />
+      <PostList postList={posts} />
       <Board onPostNext={handleNextPage} onPostPrev={handlePrevPage} />
     </>
   );
--- src/components/post/PostEdit/PostEditList.jsx
+++ src/components/post/PostEdit/PostEditList.jsx
@@ -1,20 +1,46 @@
+import { useEffect, useState } from "react";
+
 import DeleteIcon from "@/assets/icons/close.png";
-import { usePosts } from "@/context/PostContext";
+import { useDeletePost } from "@/queries/useDeletePost";
+import { useGetPosts } from "@/queries/useGetPosts";
+import { useTotalPage } from "@/queries/useTotalPage";
+import { pageStore } from "@/store/pageStore";
 
 import Pagination from "./Pagination";
 import styles from "./PostEditList.module.css";
 
 export default function PostEditList() {
-  const {
-    isLoading,
-    postList,
-    page,
-    totalPage,
-    handleDeletePost,
-    handlePageChange,
-  } = usePosts();
+  const [pageDocs, setPageDocs] = useState([null]);
+  const [page, setPage] = useState(1);
+  const { data: totalPage } = useTotalPage();
+  const { data, isFetching } = useGetPosts(page, pageDocs);
+  const { mutate: deletePost } = useDeletePost();
 
-  if (isLoading) {
+  const posts = data?.posts ?? [];
+
+  const handleDeletePost = (e, postId) => {
+    e.stopPropagation();
+    deletePost(postId);
+  };
+
+  const handlePageChange = (newPage) => {
+    if (!pageDocs[newPage - 1] && newPage !== 1) {
+      return;
+    }
+
+    setPage(newPage);
+  };
+
+  useEffect(() => {
+    if (!data?.lastDoc) return;
+    setPageDocs((prev) => {
+      const newArr = [...prev];
+      newArr[page] = data.lastDoc;
+      return newArr;
+    });
+  }, [data]);
+
+  if (isFetching) {
     return (
       <div>
         <p style={{ color: "#222" }}>Loading...</p>
@@ -24,7 +50,7 @@ export default function PostEditList() {
 
   return (
     <div className={styles.container}>
-      {postList.map((post) => (
+      {posts.map((post) => (
         <div key={post.id} className={styles.list}>
           <div className={styles.card}>
             <p className={styles.text}>{post.content}</p>
@@ -32,8 +58,7 @@ export default function PostEditList() {
           </div>
           <button
             onClick={(e) => {
-              e.stopPropagation();
-              handleDeletePost(post.id);
+              handleDeletePost(e, post.id);
             }}
             className={styles.deleteButton}
           >
