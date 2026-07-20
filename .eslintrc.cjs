710992934739d7336c8a4f6bdbb7c19e4cd7a772
JeongHyeonKim <kjh985264@gmail.com>
2026-07-20 19:41:19 +0900

    chore: ESlint 설정 복구

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,42 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+
+  env: {
+    browser: true,
+    node: true,
+    es2020: true,
+  },
+
+  ignorePatterns: ["dist", ".eslintrc.cjs", "vite.config.js"],
+
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+
   extends: [
     "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
     "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
+
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+
   plugins: ["react-refresh"],
+
   rules: {
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+
+    "react/no-unknown-property": "off",
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
