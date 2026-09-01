d27f61f12e44735e9cdebee81e159cd0e8888094
JeongHyeonKim <kjh985264@gmail.com>
2026-09-01 20:13:08 +0900

    feat: 로딩 스피너 추가 및 배럴 익스포트

--- src/components/ui/index.js
+++ src/components/ui/index.js
@@ -1,5 +1,6 @@
 export { default as Button } from "./Button/Button";
 export { default as Input } from "./Input/Input";
+export { default as Loading } from "./Loading/Loading";
 export { default as LoadingScreen } from "./Loading/LoadingScreen";
 export { default as LoadingSpinner } from "./Loading/LoadingSpinner";
 export { default as Menu } from "./Menu/Menu";
