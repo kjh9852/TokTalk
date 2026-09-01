912932dd9dd75e6580241edb8111c38ec397195b
JeongHyeonKim <kjh985264@gmail.com>
2026-09-01 21:07:09 +0900

    feat: 모바일 버튼 터치 동작 개선

--- src/components/mobile/button/MobileButton.jsx
+++ src/components/mobile/button/MobileButton.jsx
@@ -3,13 +3,15 @@ import { useMobileControlStore } from "@/store/mobileControlsStore";
 import styles from "./MobileButton.module.css";
 
 export default function MobileButton({ type, icon }) {
+  const isPressed = useMobileControlStore((state) => state.mobileAction[type]);
+
   const setMobileAction = useMobileControlStore(
     (state) => state.setMobileAction,
   );
 
   return (
     <button
-      className={`${styles.mobileButton} ${styles[type]}`}
+      className={`${styles.mobileButton} ${styles[type]} ${isPressed ? styles.active : ""}`}
       onTouchStart={() => setMobileAction({ [type]: true })}
       onTouchEnd={() => setMobileAction({ [type]: false })}
       onTouchCancel={() => setMobileAction({ [type]: false })}
