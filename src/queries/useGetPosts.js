132a7605aada41bd288376afd9957a9b17761f85
JeongHyeonKim <kjh985264@gmail.com>
2026-09-11 21:09:45 +0900

    fix 게시글 쿼리 cursor 유효성 검사 추가

--- src/queries/useGetPosts.js
+++ src/queries/useGetPosts.js
@@ -3,13 +3,14 @@ import { useQuery } from "@tanstack/react-query";
 import { getPost } from "@/api/post/post";
 
 export function useGetPosts(page, pageDocsRef) {
+  const startDoc = pageDocsRef.current[page - 1] ?? null;
+
   return useQuery({
     queryFn: () => {
-      const startDoc = pageDocsRef.current[page - 1] ?? null;
-
       return getPost({ pageSize: 6, startDoc, page });
     },
     queryKey: ["posts", page],
     staleTime: 1000 * 60,
+    enabled: page === 1 || !!startDoc,
   });
 }
