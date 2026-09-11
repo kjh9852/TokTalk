0e33cd49a5e3ee4e2fb056a43fcbce3f0260602d
JeongHyeonKim <kjh985264@gmail.com>
2026-09-11 21:10:41 +0900

    fix: 모바일 FireStore 연결 지연 개선

--- src/api/firebase/firebase.js
+++ src/api/firebase/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -21,5 +21,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+export const db = initializeFirestore(app, {
+  experimentalForceLongPolling: true,
+});
+
 export const storage = getStorage(app);
